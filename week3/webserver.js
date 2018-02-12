8cf561def07b1f39fdcc9ef4a652a1ced38d55b2
Shawn Ma <emailme.mx@gmail.com>
2018-02-11 20:36:22 -0500

    first version of contact form
    frist try of contact form for sending and displaying

--- week3/webserver.js
+++ week3/webserver.js
@@ -13,21 +13,48 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-});
-
 var count = 0;
 
-app.get('/somethingelse', function(req, res) {
-  count++;
-  // res.send('<html><body><h1> somethingelse' + count +'</h1></body></html>')
-  res.send('<html><body><h1> somethingelse' + count + '</h1></body></html>')
+// app.get('/display', function(req, res) {
+//   app.render('display.ejs', {
+//     test: 8});
+// });
+
+app.get('/emailRec', function (req,res) {
+
+  var obj = new Object();
+   obj.name = req.query.name;
+   obj.email  = req.query.email;
+   obj.content = req.query.content;
+   var contacts= JSON.stringify(obj);
+
+  // var tempString = '{"contacts" : {"name":req.query.name, "email":eq.query.email, "content":req.query.content}}'
+  db.contacts.save(JSON.parse(contacts), function(err, saved) {
+    if( err || !saved ) console.log("Not saved");
+      else console.log("Saved");
+  });
+
+  res.redirect('/display');
+
 });
 
 app.get('/display', function(req, res) {
-  app.render('display.ejs', {
-    test: 8});
+
+  db.contacts.find({}, function(err, saved) {
+    if (err || !saved) {
+    	console.log("No results");
+    }
+    else saved.forEach(function(record) {
+      res.render('display.ejs', {thedata:saved});
+    	console.log(record);
+  	});
+
+  	/* Alternatively you could loop through the records with a "for"
+  	for (var i = 0; i < saved.length; i++) {
+	  	console.log(saved[i]);
+	}
+	*/
+  });
 });
 
 // app.get('/emailRec', function (req, res) {
